568a958fb9826194980d41c48e2903eaa0246573
Dmitry Shipunov <vooxil@gmail.com>
2021-04-28 23:04:10 +0300

    cleanup repository

--- handlers.go
+++ handlers.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"GO-CRUD/books"
-	"GO-CRUD/proxy"
 	"net/http"
 
 	"github.com/jinzhu/gorm"
 )
 
 func HandleRequests(db *gorm.DB) {
-	http.HandleFunc("/proxy", proxy.Proxy)
-
 	booksStore := books.NewBooksStore(db)
 	booksService := books.NewService(booksStore)
 
